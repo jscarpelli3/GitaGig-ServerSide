4c785608dbf346ebbf4adc17c6b68641ca410278
jeremyHarrell <hillvulf@gmail.com>
2022-11-10 15:59:51 -0600

    created controllers. updated AuthController with necessary bandleader information for Register function

---
+++ routes/BandLeaderRouter.js
@@ -0,0 +1,13 @@
+const router = require('express').Router()
+const controller = require('../controllers/BandLeaderController')
+const middleware = require('../middleware')
+
+router.get('/:bandleader_id', 
+middleware.stripToken,
+middleware.verifyToken,
+controller.GetBandleaderById)
+
+router.post('/', 
+middleware.stripToken,
+middleware.verifyToken,
+controller.CreateBandleader)
